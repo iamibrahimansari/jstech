174c43655c3585e4818f24c91cc5e0ee052b0bcc
Ibrahim Ansari <iamibrahimansari@gmail.com>
2024-11-14 14:19:21 +0530

    create pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,41 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./pages/home";
+import About from "./pages/about";
+import Signup from "./pages/signup";
+import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
+import Projects from "./pages/projects";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/about",
+        element: <About />,
+    },
+    {
+        path: "/signup",
+        element: <Signup />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/dashboard",
+        element: <Dashboard />,
+    },
+    {
+        path: "/projects",
+        element: <Projects />,
+    },
+]);
+
 const App = () => {
-    return <div className="text-3xl text-red-500">App</div>;
+    return <RouterProvider router={router} />;
 };
 
 export default App;
